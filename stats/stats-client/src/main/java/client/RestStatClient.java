bd4ddffde54722fb4cda7c85e1b5478cb07fb984
Vladislav Kozlov <littlerock2877@mail.ru>
2025-06-15 21:44:23 +0300

    fix: client

--- stats/stats-client/src/main/java/client/RestStatClient.java
+++ stats/stats-client/src/main/java/client/RestStatClient.java
@@ -2,7 +2,10 @@ package client;
 
 import exception.InvalidRequestException;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,25 +13,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.dto.ViewStatsDto;
+
+import java.net.URI;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class RestStatClient implements StatClient {
+    private final DiscoveryClient discoveryClient;
 
-    private final RestClient restClient;
-    private final String statUrl;
-
-    public RestStatClient(@Value("${client.url}") String statUrl) {
-        this.statUrl = statUrl;
-        this.restClient = RestClient.builder()
-                .baseUrl(statUrl)
-                .build();
-    }
+    @Value("${service.stats-server}")
+    private String statServerId;
 
     @Override
     public void saveHit(EndpointHitDto endpointHitDto) {
-        restClient.post()
-                .uri("/hit")
+        URI serviceUri = getStatsServiceUri();
+        RestClient.create().post()
+                .uri("%s/hit".formatted(serviceUri))
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(endpointHitDto)
                 .retrieve()
@@ -39,21 +40,30 @@ public class RestStatClient implements StatClient {
 
     @Override
     public List<ViewStatsDto> getStats(String start, String end, List<String> uris, Boolean unique) {
-        try {
-            return restClient.get()
-                    .uri(uriBuilder -> uriBuilder.path("/stats")
-                            .queryParam("start", start)
-                            .queryParam("end", end)
-                            .queryParam("uris", uris)
-                            .queryParam("unique", unique)
-                            .build())
-                    .retrieve()
-                    .onStatus(status -> status != HttpStatus.OK, (request, response) -> {
-                        throw new InvalidRequestException(response.getStatusCode().value() + ": " + response.getBody());
-                    })
-                    .body(new ParameterizedTypeReference<List<ViewStatsDto>>() {});
-        } catch (Exception e) {
-            throw new InvalidRequestException(e.getMessage());
+        RestClient client = RestClient.builder()
+                .baseUrl(getStatsServiceUri().toString())
+                .build();
+
+        return client.get()
+                .uri(uriBuilder -> uriBuilder.path("/stats")
+                        .queryParam("start", start)
+                        .queryParam("end", end)
+                        .queryParam("uris", uris)
+                        .queryParam("unique", unique)
+                        .build())
+                .retrieve()
+                .onStatus(status -> status != HttpStatus.OK, (request, response) -> {
+                    throw new InvalidRequestException(response.getStatusCode().value() + ": " + response.getBody());
+                })
+                .body(new ParameterizedTypeReference<List<ViewStatsDto>>() {});
+    }
+
+    private URI getStatsServiceUri() {
+        List<ServiceInstance> instances = discoveryClient.getInstances(statServerId);
+        if (instances.isEmpty()) {
+            throw new IllegalStateException("Service %s not found in Eureka".formatted(statServerId));
         }
+        ServiceInstance serviceInstance = instances.getFirst();
+        return serviceInstance.getUri();
     }
 }
