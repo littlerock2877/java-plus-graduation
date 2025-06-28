be35228e0ced93e2ac624608d2ce66506df576e4
Vladislav Kozlov <littlerock2877@mail.ru>
2025-06-28 16:07:49 +0300

    fix: users

--- core/user-service/src/main/java/ru/practicum/user/UserServiceApp.java
+++ core/user-service/src/main/java/ru/practicum/user/UserServiceApp.java
@@ -3,7 +3,7 @@ package ru.practicum.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"client", "ru.practicum.event"})
+@SpringBootApplication
 public class UserServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApp.class, args);
