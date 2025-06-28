99718b7cdf61032aa6a0123e3db64fae25762ef5
Vladislav Kozlov <littlerock2877@mail.ru>
2025-06-28 14:28:36 +0300

    add: request and user service

---
+++ core/request-service/src/main/java/ru/practicum/request/RequestServiceApp.java
@@ -0,0 +1,11 @@
+package ru.practicum.request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RequestServiceApp.class, args);
+    }
+}
