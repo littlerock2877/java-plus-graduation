cb922f20ecfa4a25bd93f9afd29569a6f871bc99
Vladislav Kozlov <littlerock2877@mail.ru>
2025-07-05 18:51:17 +0300

    rem:client

--- core/request-service/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
+++ core/request-service/src/main/java/ru/practicum/request/controller/RequestPrivateController.java
@@ -35,6 +35,7 @@ public class RequestPrivateController {
         log.info("Creating request by user with id {} for event with id {} - Started", userId, eventId);
         RequestDto requestDto = requestService.createRequest(userId, eventId);
         log.info("Creating request by user with id {} for event with id {} - Finished", userId, eventId);
+        restStatClient.collectUserAction(userId, eventId, UserActionType.REGISTER);
         return requestDto;
     }
 
